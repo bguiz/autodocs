00d879c339933bab7b0e76e06cc3c5920dca3a68
bguiz <brendan.graetz@gmail.com>
2015-04-22 18:24:57 +1000

    =BG= fully document self!

--- environment-variables.js
+++ environment-variables.js
@@ -1,16 +1,45 @@
 'use strict';
 
+/**
+ * @class  EnvironmentVariables
+ * @module  Autodocs
+ */
 
+/**
+ * Test that an environment variable exists
+ *
+ * @method exists
+ * @for  EnvironmentVariables
+ * @param  {String} name The name of the environment variable
+ * @return {Boolean} `true` when environment variable is defined and **is not** an empty string
+ */
 function existsEnvironmentVariable(name) {
   return !!process.env[name];
 }
 
+/**
+ * Enforce that an environment variable exists
+ *
+ * @method require
+ * @for  EnvironmentVariables
+ * @param  {String} name The name of the environment variable
+ * @throws  Throws and error when the environment variable does not exist
+ */
 function requireEnvironmentVariable(name) {
   if (!existsEnvironmentVariable(name)) {
     throw new Error('Environment variable `'+name+'` not set');
   }
 }
 
+/**
+ * Set a default value for an environment variable if it does not exist
+ *
+ * @method default
+ * @for  EnvironmentVariables
+ * @param  {String} name The name of the environment variable
+ * @param  {String} value The default value
+ * @return {String} The value of the existing or newly set environment variable
+ */
 function defaultEnvironmentVariable(name, value) {
   if (!existsEnvironmentVariable(name)) {
     process.env[name] = value;
@@ -18,6 +47,32 @@ function defaultEnvironmentVariable(name, value) {
   return process.env[name];
 }
 
+/**
+ * Substitute the value of other environment variables into the named one.
+ *
+ * For example:
+ *
+ * ```javascript
+ * process.env.MAJOR_VERSION = "0";
+ * process.env.MINOR_VERSION = "3";
+ * EnvironmentVariables.substitute('api/{{MAJOR_VERSION}}.{{MINOR_VERSION}}');
+ * ```
+ *
+ * ... will return `'api/0.3'`;
+ *
+ * Note that if this is done for multiple environment variables,
+ * the order in which they are done will affect the result.
+ * It is the responsibility of the caller to this function to ensure that
+ * the values to be substituted in are already fully resolved:
+ * this function **is not** recursive.
+ * Therefore, cycles are also not allowed.
+ * Any of these will result in indeterminate results, including errors.
+ *
+ * @method default
+ * @for  EnvironmentVariables
+ * @param  {String} name The name of the environment variable
+ * @return {String} The value of the existing or newly set environment variable
+ */
 function substituteEnvironmentVariable(name) {
   process.env[name] = process.env[name]
     .replace(/{{[^{}]+}}/g, function(otherName) {
@@ -25,6 +80,7 @@ function substituteEnvironmentVariable(name) {
       requireEnvironmentVariable(otherName);
       return process.env[otherName];
     });
+  return process.env[name];
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,85 +1,15 @@
 'use strict';
 
-var path = require('path');
-
-var envVar = require('./environment-variables');
+var autodocs = require('./autodocs');
 
 /**
+ * The main entry point for being invoked from npm scripts
+ * (or even from the command line)
+ *
+ * Simply does `autodocs.run()`
+ *
+ * @class  AutodocsRun
  * @module  Autodocs
  */
 
-/**
- * @class  Autodocs
- */
-
-autodocs();
-
-/**
- * The main entry point.
- *
- * Will determine which continuous integration environment and publishing environment to use,
- * and then run those appropriately.
- * Currently, the supported ones are:
- *
- * - Continuous Integration (`SELECT_CI`):
- *   - `travis`
- * - Publishing Environment (`SELECT_PUBLISH`):
- *   - `github-pages`
- *
- * Runs various environment variables checks and sets defaults where appropriate.
- * Then tests whether this particular build should trigger publishing the documentation,
- * and if so fires the publish script
- *
- * @method autodocs
- * @for  Autodocs
- */
-function autodocs() {
-  var ciName = envVar.default('SELECT_CI', 'travis');
-  var publishName = envVar.default('SELECT_PUBLISH', 'github-pages');
-  var ci = require('./ci/'+ciName);
-  var publish = require('./publish/'+publishName);
-  ci.init();
-  publish.init();
-  environmentVariablesCommon();
-  if (ci.shouldRun()) {
-    console.log('This build will generate new documentation');
-    publish.run();
-  }
-  else {
-    console.log('This build does not need to generate new documentation');
-  }
-}
-
-function environmentVariablesCommon() {
-  var projectPath = path.resolve('.');
-  console.log('projectPath', projectPath);
-  var projectPackageJson = require(path.resolve(projectPath, 'package.json'));
-  var projectVersion = projectPackageJson.version;
-  process.env.PROJECT_DIR = projectPath;
-  process.env.MAJOR_MINOR_PATCH_VERSION = projectVersion;
-  var projectVersionTokens = projectVersion.split('.');
-  process.env.MAJOR_VERSION = projectVersionTokens[0];
-  process.env.MINOR_VERSION = projectVersionTokens[1];
-  process.env.PATCH_VERSION = projectVersionTokens.slice(2).join('.');
-
-  envVar.default('GIT_USER', 'autodocs Git User');
-  envVar.default('GIT_EMAIL', 'autodocs-git-user@bguiz.com');
-
-  envVar.default('FLAG_COPY_ASSETS', 'false');
-  envVar.default('FLAG_PUBLISH_ON_RELEASE', 'false');
-  envVar.default('FLAG_CLEAN_DOCUMENT', 'false');
-  envVar.default('FLAG_STRIP_TOKEN_OUTPUT', 'true');
-
-  envVar.default('DOCUMENT_BRANCH', 'master');
-  envVar.default('DOCUMENT_JOB_INDEX', '1');
-  envVar.default('DOCUMENT_GENERATED_FOLDER', 'documentation');
-  envVar.default('DOCUMENT_PUBLISH_FOLDER', 'api/{{MAJOR_VERSION}}.{{MINOR_VERSION}}');
-  envVar.default('DOCUMENT_ASSETS', '');
-
-  //NOTE order of the values contained in the array matters -
-  // the ones that run first should require the ones that run later to be fully resolved first
-  // and of course, cycles will result in indeterminate results.
-  [
-    'DOCUMENT_PUBLISH_FOLDER'
-  ].forEach(envVar.substitute);
-}
+autodocs.run();
