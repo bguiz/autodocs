173a0433c932bd698c55caf1153405cc32419137
autodocs Git User <autodocs-git-user@bguiz.com>
2015-04-22 05:50:30 +0000

    autodocs publish 2015-04-22-05-50-30 327cae9

---
+++ api/0.3/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Autodocs"
+    ],
+    "modules": [
+        "Autodocs"
+    ],
+    "allModules": [
+        {
+            "displayName": "Autodocs",
+            "name": "Autodocs"
+        }
+    ]
+} };
+});
