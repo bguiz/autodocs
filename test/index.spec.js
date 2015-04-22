433d4e0864c7cf0aa647e47d44b65e66dd9f6b29
bguiz <brendan.graetz@gmail.com>
2015-04-22 12:02:12 +1000

    =BG= fix tests to remove one of the required environment variables

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,12 @@
 
 describe('[index]', function() {
   it('Should require env vars in order to run', function(done) {
+    var GH_TOKEN = process.env.GH_TOKEN;
+    process.env.GH_TOKEN = undefined;
     expect(function() {
       require('../index');
     }).toThrowError( /Environment variable `[^`]+` not set/);
+    process.env.GH_TOKEN = GH_TOKEN;
     done();
   });
 });
