0585ab6135fec100c5db782ed2acb1f95c22ce56
Brendan Graetz <github@bguiz.com>
2015-04-23 17:30:24 +1000

    =BG= tests to verify that execution fails when compulsory environment variables are not present

--- test/run.spec.js
+++ test/run.spec.js
@@ -1,5 +1,32 @@
 'use strict';
 
+var envs = {
+  buildOnBranch: function() {
+    return {
+      TRAVIS_REPO_SLUG: 'bguiz/autodocs',
+      TRAVIS_PULL_REQUEST: 'false',
+      TRAVIS_BRANCH: 'master',
+      TRAVIS_BUILD_NUMBER: 'foo',
+      TRAVIS_JOB_NUMBER: 'foo.1',
+      GH_TOKEN: 'unicorns',
+      FLAG_SKIP_PUSH: 'true',
+    };
+  },
+  buildOnRelease: function() {
+    return {
+      TRAVIS_REPO_SLUG: 'bguiz/autodocs',
+      TRAVIS_PULL_REQUEST: 'false',
+      TRAVIS_BRANCH: 'unicorn-branch',
+      TRAVIS_BUILD_NUMBER: 'foo',
+      TRAVIS_JOB_NUMBER: 'foo.1',
+      TRAVIS_TAG: 'anythingotherthanfalse',
+      FLAG_PUBLISH_ON_RELEASE: 'true',
+      GH_TOKEN: 'unicorns',
+      FLAG_SKIP_PUSH: 'true',
+    };
+  },
+};
+
 describe('[run]', function() {
   describe('[validation]', function() {
     var savedEnv;
@@ -13,6 +40,7 @@ describe('[run]', function() {
       process.env = savedEnv;
       savedEnv = undefined;
     });
+
 
     it('Should verify that environment is empty', function(done) {
       expect(process.env).toEqual({});
@@ -41,5 +69,27 @@ describe('[run]', function() {
       }).toThrowError( /Cannot find module \'[^\']+\'/ );
       done();
     });
+
+    describe('[compulsory tokens]', function() {
+      [
+        'GH_TOKEN',
+        'TRAVIS_REPO_SLUG',
+        'TRAVIS_PULL_REQUEST',
+        'TRAVIS_BRANCH',
+        'TRAVIS_BUILD_NUMBER',
+        'TRAVIS_JOB_NUMBER',
+      ].forEach(function(name) {
+        it('Should fail when '+name+' is not set', function(done) {
+          process.env = envs.buildOnBranch();
+          process.env[name] = undefined;
+          expect(function() {
+            require('../autodocs').run();
+          }).toThrowError(new RegExp('Environment variable `'+name+'` not set'));
+          done();
+        });
+      });
+    });
+
+
   });
 });
