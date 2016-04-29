d6e348636fb1ed61621198ac408cd88a1b0247e5
autodocs Git User <autodocs-git-user@bguiz.com>
2016-04-29 23:51:27 +0000

    autodocs publish 2016-04-29-23-51-26 8d98875

---
+++ api/0.9/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Autodocs",
+        "AutodocsRun",
+        "CiTravis",
+        "EnvironmentVariables",
+        "PublishGithubPages"
+    ],
+    "modules": [
+        "Autodocs",
+        "AutodocsCi",
+        "AutodocsPublish"
+    ],
+    "allModules": [
+        {
+            "displayName": "Autodocs",
+            "name": "Autodocs"
+        },
+        {
+            "displayName": "AutodocsCi",
+            "name": "AutodocsCi"
+        },
+        {
+            "displayName": "AutodocsPublish",
+            "name": "AutodocsPublish"
+        }
+    ]
+} };
+});
